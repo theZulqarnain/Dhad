7f9f36e20b573d790f03921ab2318f88cfb4f05d
zulqarnain <ak.zul65@gamil.com>
2019-07-30 14:49:11 +0530

    previous sentence editing functionality has done

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,33 +12,43 @@ const Main = (props) =>{
         segmentingEnterdText(e)
     }
     const segmentingEnterdText = (text) => {
-        if(text === oldText) return;
-        
-        
-        let sentence = text.split('.');;
+        let sentences = text.split('.');
         let lastChar = text.charCodeAt(text.length -1);
         let lastSentence = {};
-        console.log(sentence,text);
-
         
-        if(lastChar === 46 && (sentence.length) -2 === index){
-            lastSentence[index] = sentence[(sentence.length)-2];
+        if(lastChar === 46 && (sentences.length) -2 === index){
+            //this condition enter new value in segment obj if . enter
+            lastSentence[index] = sentences[(sentences.length)-2];
             Object.assign(segment,lastSentence)
             index++;
-        }else{
-            if((sentence.length)-1 === index){
-                lastSentence[index] = sentence[sentence.length-1];
+        }else if((sentences.length)-1 === index){
+            let latestSentence = Object.keys(segment).length-1;
+            if(segment[latestSentence] !== sentences[sentences.length-1]){
+                //this condition will enter text to last element of segment
+                lastSentence[index] = sentences[sentences.length-1];
                 Object.assign(segment,lastSentence)
             }else{
-                lastSentence[sentence.length-1] = sentence[sentence.length-1];
-                Object.assign(segment,lastSentence);
-                delete segment[index]; 
-                index--;
+                //this condition handle if someone change previous sentencess
+                let segmentVals = []
+                for(let key in segment){
+                    segmentVals.push(segment[key])
+                }
+                for(let i=0;i < segmentVals.length;i++){
+                    if(segmentVals[i] !== sentences[i]){
+                        segmentVals[i] = sentences[i];
+                        segment[i] =sentences[i];
+                    }
+                }
             }
             
         }
-        console.log(segment)
-        
+        else{
+            //this condition handles if someone removing text from sentencess
+            lastSentence[sentences.length-1] = sentences[sentences.length-1];
+            Object.assign(segment,lastSentence);
+            delete segment[index]; 
+            index--;
+        }
     }
     
     return (
