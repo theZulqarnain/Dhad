79491d7172776626d57de2574ffeb0f5343f6d27
Rami Bamaas <ramibnkseel@gmail.com>
2019-08-06 06:55:33 +0530

    Update Main.js
    adding the changes to main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,38 +1,101 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux';
-import {dataListHandler} from '../action';
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { dataListHandler } from '../action';
+import ContentEditable from 'react-contenteditable';
 
-const Main = (props) =>{
-    const dataListPass = (e) =>{
-        console.log(e);
-        // props.dataListHandler(['third','fourth'])
-        let lastChar = e.charCodeAt(e.length -1);
-        if(lastChar === 46){
-            console.log(lastChar,"we end this sentence");
-        }
-        // console.log(e.charCodeAt(e.length -1))
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      html: "<div>Enter something here!</div>"
+    };
+    this.callAPI = this.callAPI.bind(this);
+   
+  }
+
+  handleChange = evt => {
+   
+    this.setState({ html: evt.target.value });
+    this.callAPI(evt.target.value);
+  };
+
+  
+
+
+  HighlighterHandler =  (text ,  wrongWord ) => {
+
+    let  diacritics = "[\u064E\u064B\u064F\u064C\u0650\u064D\u0651\u0652ـ]*";
+    let  splitedCharacter = "";
+    for (var i = 0; i < wrongWord.length; i++) {
+      let  character      = wrongWord.charAt(i);
+      splitedCharacter += character + diacritics ;
+    
     }
+    
+    let words = [splitedCharacter];
+    let pattern = "(" + words.join("|") + ")"; 
+        pattern = "(^|\\s)" + pattern + "($|[/_{}:,\\s\\-\\[\\]]+)";
+    let regex = new RegExp(pattern, "m");
+    let match = text.match(regex);
+    let matchedWord = "";
+    if (match) {
+      matchedWord = match[0];
+      let startPostion = match.index + match[1].length;
+      let endPostion = match[2].length ; 
+
+      
+     
+    }
+
+
+     return matchedWord 
+  } 
+  
+  callAPI = (text) => {
+    console.log("API get called!", text);
+    // هَذَا كِتَابِ مُفِيدِ.
+    // after api calls
+    let wrongWords = ['هذا'];
+    let suggestedList = {
+      'wrong1': ['wrong'] , 
+      'wrong2': ['wrong']  ,
+      'wrong3': ['wrong'] 
+    };
+
+    for( let i = 0; i < wrongWords.length; i++) {
+      let wrongWord = wrongWords[i];
+      /* for now I did not use wrongWrod_id and wrongWord_suggestedList but we might need them */
+      let wrongWord_id = "error-" + i ;
+      let wrongWord_suggestedList = suggestedList[wrongWord];
+      this.HighlighterHandler( text  , wrongWord );
+    };
+
+          
+  }
+
+  render = () => {
+    
     return (
-        <div className="Editor">
-            <h2>Dhad Editor</h2>
-            <div 
-                contentEditable={true} 
-                suppressContentEditableWarning={true} 
-                spellCheck="false" 
-                className="dhad_editor"
-                onInput={(e)=>dataListPass(e.currentTarget.textContent)}
-                onBlur={(e)=>dataListPass(e.currentTarget)}
-            >
-                Please write something
-            </div>
-            <button onClick={()=>dataListPass()}>submit</button>
-        </div>
-    )
+      <div className="Editor">
+        <h2>Dhad Editor</h2>
+        <ContentEditable
+          html={this.state.html} // innerHTML of the editable div
+          disabled={false} // use true to disable edition
+          onChange={this.handleChange} // handle innerHTML change
+          className="dhad_editor"
+        />
+        <button onClick={() => dataListPass()}>submit</button>
+      </div>
+
+    );
+  };
 }
 
-const mapStateToProps = (state) =>{
-    return{
-        DataList:state.MainReducer.data_list
-    }
+
+
+const mapStateToProps = (state) => {
+  return {
+    DataList: state.MainReducer.data_list
+  }
 }
-export default connect(mapStateToProps,{dataListHandler})(Main);
+export default connect(mapStateToProps, { dataListHandler })(Main);
